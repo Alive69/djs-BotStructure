519473c70e3099805e9a5a619fa815a81c04ab1f
Alive <100870423+Alive69@users.noreply.github.com>
2022-05-17 10:22:30 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,11 +1,1 @@
-const mongoDB = require(process.env.mongoDB)
-const { Client } = require('discord.js')
 
-/**
- * @param { Client } client
- */
-
-client.on('ready', () => {
-    console.log(`${client.user.tag} is online!`)
-    client.user.setActivity('Netcord', { type: 'WATCHING' })
-})
