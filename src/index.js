026a7aca356d175cd01f65dd2949ad97188f178f
Alive <100870423+Alive69@users.noreply.github.com>
2022-05-18 11:42:03 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: 32767 });
+const fs = require('fs');
+require('dotenv').config();
+
+client.commands = new Discord.Collection();
+
+const handlers = fs.readdirSync('./src/Handlers').filter(file => (file.endsWith('.js')));
+const commandFolders = fs.readdirSync('./src/Commands');
+const eventFiles = fs.readdirSync('./src/Events').filter(file => (file.endsWith('.js')));
+
+(async () => {
+    for(file of handlers) {
+        require(`./Handlers/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, './src/Events');
+    client.handleCommands(commandFolders, './src/Commands');
+    client.dbLogin();
+    client.login(process.env.TOKEN);
+})();
